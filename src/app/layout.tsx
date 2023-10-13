b8ebf8f850c8de01a912a25669f1023aca49527a
joaoeduardodias <joaoeduardodias123@gmail.com>
2023-10-13 14:44:52 -0300

    feat: created section cards

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Sidebar } from './components/Sidebar'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'CRM Frigosul',
+  title: 'CRM ',
   description: 'CRM Frigosul SulBeef',
 }
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} flex bg-white`}>
+        <Sidebar />
+        {children}
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,39 @@
+import { CircleDollarSign, LineChart, Plus, Users2 } from 'lucide-react'
+import { Header } from './components/Header'
 
 export default function Home() {
-  return <h1>hello word</h1>
+  return (
+    <div className="flex-1">
+      <Header />
+      <section className="ml-4 mt-4 flex items-center space-x-4">
+        <div className="min-w-[15rem] space-y-4 rounded-md border border-slate-300 bg-white p-5 shadow-sm">
+          <div className="flex items-center justify-between">
+            <strong className="text-xs font-normal">Valor total</strong>
+            <CircleDollarSign className="h-4 w-4 text-zinc-500" />
+          </div>
+          <p className="flex items-center  text-lg font-semibold ">
+            R$: 180.000,00
+          </p>
+        </div>
+        <div className="min-w-[15rem] space-y-4 rounded-md border border-slate-300 bg-white p-5 shadow-sm">
+          <div className="flex items-center justify-between">
+            <strong className="text-xs font-normal">Clientes Atendidos</strong>
+            <Users2 className="h-4 w-4 text-zinc-500" />
+          </div>
+          <p className="flex items-center  text-lg font-semibold ">
+            <Plus /> 200
+          </p>
+        </div>
+        <div className="min-w-[15rem] space-y-4 rounded-md border border-slate-300 bg-white p-5 shadow-sm">
+          <div className="flex items-center justify-between">
+            <strong className="text-xs font-normal">Vendas Realizadas</strong>
+            <LineChart className="h-4 w-4 text-zinc-500" />
+          </div>
+          <p className="flex items-center  text-lg font-semibold ">
+            <Plus /> 150
+          </p>
+        </div>
+      </section>
+    </div>
+  )
 }
