112f2928f05886d67b1dfa4d69c63e6d62f97b60
joaoeduardodias <joaoeduardodias123@gmail.com>
2023-10-21 11:58:55 -0300

    feat: add logo

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,9 @@
 'use client'
 import * as Collapsible from '@radix-ui/react-collapsible'
 import { Menu, X } from 'lucide-react'
+import Image from 'next/image'
 import { useState } from 'react'
+import logoSulbeef from '../../assets/logo.png'
 import { NavItem } from './NavItem'
 export function Sidebar() {
   const [open, setOpen] = useState<boolean>(false)
@@ -14,19 +16,32 @@ export function Sidebar() {
     <Collapsible.Root
       open={open}
       onOpenChange={setOpen}
-      className="fixed left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b bg-white p-4 data-[state=open]:bottom-0 lg:relative lg:right-auto lg:w-40 lg:border-r lg:py-8 lg:pb-0 lg:pl-8  lg:pr-0 lg:data-[state=closed]:bottom-0"
+      className="8 fixed left-0 right-0 top-0 z-20 flex flex-col gap-6  border-b bg-white  p-4 data-[state=open]:bottom-0 lg:relative lg:right-auto lg:w-40 lg:border-r lg:p-0 lg:data-[state=closed]:bottom-0"
     >
-      <div className="flex items-center justify-between">
-        <p className=" w-full text-center">Logo</p>
-        <Collapsible.Trigger asChild className="lg:hidden">
+      <div className="flex  items-center justify-center px-2 lg:justify-between">
+        <Image
+          src={logoSulbeef}
+          alt="Logo Sulbeef"
+          className="w-[7rem] px-2 md:w-[10rem] lg:w-full lg:pt-4"
+          width="0"
+          height="0"
+        />
+        <Collapsible.Trigger
+          asChild
+          className="absolute right-4 top-6 md:right-5 md:top-9 lg:hidden"
+        >
           <button>
-            {open ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
+            {open ? (
+              <X className="h-6 w-6 md:h-8 md:w-8" />
+            ) : (
+              <Menu className="h-6 w-6 md:h-8 md:w-8" />
+            )}
           </button>
         </Collapsible.Trigger>
       </div>
       <Collapsible.Content
         forceMount
-        className="flex flex-1 flex-col items-center justify-center gap-6  data-[state=closed]:hidden  lg:mt-6 lg:items-start lg:justify-normal lg:data-[state=closed]:flex"
+        className="flex flex-1  flex-col items-center justify-center gap-6 data-[state=closed]:hidden  lg:mt-6  lg:items-start lg:justify-normal lg:pl-8 lg:data-[state=closed]:flex"
       >
         <NavItem
           address="/"
